e5c82771d031170979567a6a616572c367e2ca89
Yeonhee Kim <qkfh559@gmail.com>
2024-10-14 16:14:47 +0900

    modify eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,21 @@
-module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
+// eslint.config.js (Flat Config 형식)
+import { flat } from "eslint-config-flat";
+
+export default [
+  {
+    files: ["*.js"],
+    languageOptions: {
+      globals: {
+        // 여기에 환경 변수를 정의합니다.
+        console: "readonly",
+        window: "readonly",
+        document: "readonly",
+        // 필요한 환경 추가
+      },
     },
     rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'double'],
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
     },
-  };
+  },
+];
